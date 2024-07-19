5be491a9d4bcf2b8c0b4d1630ddd0ec4db804b3b
saeedahmed786 <119362834+devuplio@users.noreply.github.com>
2024-07-19 17:53:07 +0500

    updated backend

--- server.js
+++ server.js
@@ -49,9 +49,9 @@ mongoose.connect(config.MONGO_URI, {
 // if (process.env.NODE_ENV === 'production') {
 //     app.use(express.static('./client/build'));
 
-// app.get('/*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-// });
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 // }
 
 // Function to parse date and time in Pakistan time zone
